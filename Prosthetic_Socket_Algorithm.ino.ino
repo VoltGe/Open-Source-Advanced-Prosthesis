ac414604112fb379b91ef58f2da168921c3c8927
Joshua Belofsky <joshuabelofsky@gmail.com>
2018-07-08 18:41:29 -0700

    Add files via upload

---
+++ Prosthetic_Socket_Algorithm.ino.ino
@@ -0,0 +1,64 @@
+ #include <Servo.h>
+ Servo servo1;
+ Servo servo2;
+ Servo servo3;
+ int reading1;   
+ int reading2; 
+ int reading3; 
+
+int sensorPin  = A0;    // select the input pin for the battery
+int sensorValue = 0;  // variable to store the value coming from the sensor(s)
+int haptic = 15;      // select the pin for the haptic
+
+void setup(void) {
+  
+  pinMode(haptic, OUTPUT);  ///// for battery
+  // //BMS (Battery Management System)
+  sensorValue = analogRead(sensorPin); //.0049 volts (4.9 mV) per unit 
+  if (A0 == 378) 
+ {
+ digitalWrite (haptic, HIGH); delay(200); 
+ digitalWrite (haptic, HIGH); delay(200);
+ delay(60*1000*45);
+ }
+  
+  if (A0 == 189) ; //1010 = 4.9 volts (about)
+{
+digitalWrite (haptic, HIGH); delay(200);
+digitalWrite (haptic, HIGH); delay(200);
+digitalWrite (haptic, HIGH); delay(200);
+digitalWrite (haptic, HIGH); delay(200); 
+delay(60*1000*45);
+}   
+   if (189<A0<378) digitalWrite (haptic, LOW);
+   if (378<A0) digitalWrite (haptic, LOW);
+   if (189>A0) digitalWrite (haptic, LOW);
+ 
+ //---------------------------------------------------------------------
+ Serial.begin(9600);   
+ servo1.attach(9); //servo at digital pin 9
+ servo1.write(0); //initial point for servo
+ servo2.attach(10); //servo at digital pin 10
+ servo2.write(0); //initial point for servo
+ servo3.attach(11); //servo at digital pin 11
+ servo3.write(0); //initial point for servo
+
+}
+ 
+void loop(void) {
+  reading1 = analogRead(A1); //top fsr
+  reading2 = analogRead(A2); //middle fsr
+  reading3 = analogRead(A3); //bottom fsr
+ 
+ 
+  int value1 = map(reading1, 0, 500, 135, 90); //side servo
+  servo1.write(value1);
+
+  int value2 = map(reading2, 0, 600, 160, 120); //middle servo
+  servo2.write(value2);
+
+  int value3 = map(reading3, 0, 500, 135, 90); //side servo
+  servo3.write(value3);
+ 
+  delay(50);
+}
---
+++ Prosthetic_Socket_Algorithm_2.0.ino
@@ -0,0 +1,45 @@
+  int sensorPin1 = A0;    // select the input pin for temp ic           -------->  TOP
+  int sensorPin2 = A1;    // select the input pin for temp ic           -------->  MIDDLE
+  int sensorPin3 = A2;    // select the input pin for temp ic           -------->  BOTTOM
+  int sensorValue = 0;
+
+  
+  int tecpin1 = 1;      // select the pin for the tec1           -------->  TOP
+  int tecpin2 = 2;      // select the pin for the tec1           -------->  TOP
+  int tecpin3 = 3;      // select the pin for the tec2           -------->  MIDDLE
+  int tecpin4 = 4;      // select the pin for the tec2           -------->  MIDDLE
+  int tecpin5 = 5;      // select the pin for the tec3           -------->  BOTTOM
+  int tecpin6 = 6;      // select the pin for the tec3           -------->  BOTTOM
+#define aref_voltage 3.3         // we tie 3.3V to ARef and measure it with a multimeter!
+
+void setup() {
+  
+  //declare tec's as outputs
+  pinMode(tecpin1, OUTPUT);
+  pinMode(tecpin2, OUTPUT);
+  pinMode(tecpin3, OUTPUT);
+  pinMode(tecpin4, OUTPUT);
+  pinMode(tecpin5, OUTPUT);
+  pinMode(tecpin6, OUTPUT);
+}
+
+void loop() {
+  /////////////////TMP36'S
+  
+  sensorValue = analogRead(sensorPin1);
+  if (A0 < 70) digitalWrite (tecpin1, HIGH) ; // 35 degrees fahrenheit
+  if (A0 > 95) digitalWrite (tecpin2, HIGH) ; // 85 degrees fahrenheit
+  else digitalWrite (tecpin1 & tecpin2, LOW);
+ 
+  sensorValue = analogRead(sensorPin2);
+  if (A1 < 70) digitalWrite (tecpin3, HIGH) ; 
+  if (A1 > 95) digitalWrite (tecpin4, HIGH) ; 
+  else digitalWrite (tecpin3 & tecpin4, LOW);
+
+  
+  sensorValue = analogRead(sensorPin3);
+  if (A2 < 70) digitalWrite (tecpin5, HIGH) ; 
+  if (A2 > 95) digitalWrite (tecpin6, HIGH) ; 
+  else digitalWrite (tecpin5 & tecpin6, LOW);
+  
+}
